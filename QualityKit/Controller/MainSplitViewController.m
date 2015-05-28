3c488c92734dfee076141856d90cc307ace203fe
Qin Yubo <xnth97@live.com>
2015-05-28 23:57:19 +0800

    Add a weird log in view to meet the requirements

--- QualityKit/Controller/MainSplitViewController.m
+++ QualityKit/Controller/MainSplitViewController.m
@@ -8,6 +8,8 @@
 
 #import "MainSplitViewController.h"
 #import "ALActionBlocks.h"
+#import "MsgDisplay.h"
+#import <LocalAuthentication/LocalAuthentication.h>
 
 @interface MainSplitViewController ()
 
@@ -20,22 +22,7 @@
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     
-    /*
-    loginView = [[UIView alloc] initWithFrame:self.view.frame];
-    loginView.backgroundColor = [UIColor whiteColor];
-    
-    
-    
-    UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
-    [btn setTitle:@"Log In" forState:UIControlStateNormal];
-    [btn setFrame:CGRectMake(200, 200, 300, 80)];
-    [btn handleControlEvents:UIControlEventTouchUpInside withBlock:^(id weakSender) {
-        [loginView removeFromSuperview];
-    }];
-    [loginView addSubview:btn];
-    
-    [self.view addSubview:loginView];
-     */
+    [self initializeLoginView];
 }
 
 - (void)viewDidLoad {
@@ -47,6 +34,101 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)initializeLoginView {
+    loginView = [[UIView alloc] initWithFrame:self.view.frame];
+    loginView.backgroundColor = [UIColor whiteColor];
+    
+    UITextField *usernameField = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, 1, 1)];
+    usernameField.borderStyle = UITextBorderStyleRoundedRect;
+    usernameField.placeholder = @"请输入用户名";
+    usernameField.translatesAutoresizingMaskIntoConstraints = NO;
+    usernameField.clearButtonMode = UITextFieldViewModeWhileEditing;
+    [loginView addSubview:usernameField];
+    [usernameField becomeFirstResponder];
+    
+    UITextField *passwdField = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, 1, 1)];
+    passwdField.borderStyle = UITextBorderStyleRoundedRect;
+    passwdField.placeholder = @"请输入密码";
+    passwdField.secureTextEntry = YES;
+    passwdField.translatesAutoresizingMaskIntoConstraints = NO;
+    passwdField.clearButtonMode = UITextFieldViewModeWhileEditing;
+    [loginView addSubview:passwdField];
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [btn setTitle:@"登录" forState:UIControlStateNormal];
+    [btn setFrame:CGRectMake(0, 0, 1, 1)];
+    [btn handleControlEvents:UIControlEventTouchUpInside withBlock:^(id weakSender) {
+        if ([usernameField.text isEqualToString:@""] || [passwdField.text isEqualToString:@""]) {
+            [MsgDisplay showErrorMsg:@"请填写完整用户名和密码"];
+        } else if ([usernameField.text isEqualToString:@"syx"] && [passwdField.text isEqualToString:@"syxsb"]) {
+            [UIView animateWithDuration:0.5 animations:^{
+                loginView.center = CGPointMake(loginView.center.x, loginView.center.y + self.view.frame.size.height);
+            } completion:^(BOOL finished) {
+                if (finished) {
+                    [loginView removeFromSuperview];
+                }
+            }];
+        } else {
+            [MsgDisplay showErrorMsg:@"用户名或密码不正确"];
+        }
+        
+    }];
+    btn.translatesAutoresizingMaskIntoConstraints = NO;
+    [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    btn.backgroundColor = [UIColor colorWithRed:0.000 green:0.400 blue:0.800 alpha:1.000];
+    btn.layer.cornerRadius = 5.0;
+    btn.clipsToBounds = YES;
+    [loginView addSubview:btn];
+    
+    UIButton *touchIdBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [touchIdBtn setTitle:@"Touch ID" forState:UIControlStateNormal];
+    [touchIdBtn setFrame:CGRectMake(0, 0, 1, 1)];
+    [touchIdBtn handleControlEvents:UIControlEventTouchUpInside withBlock:^(id weakSender) {
+        LAContext *context = [[LAContext alloc] init];
+        context.localizedFallbackTitle = @"";
+        NSError *error = [[NSError alloc] init];
+        if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
+            [context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:@"请验证指纹" reply:^(BOOL success, NSError *error) {
+                if (success) {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        [UIView animateWithDuration:0.5 animations:^{
+                            loginView.center = CGPointMake(loginView.center.x, loginView.center.y + self.view.frame.size.height);
+                        } completion:^(BOOL finished) {
+                            if (finished) {
+                                [loginView removeFromSuperview];
+                            }
+                        }];
+                    });
+                } else {
+                    if (error.code == -1) {
+                        [MsgDisplay showErrorMsg:@"指纹验证失败"];
+                    }
+                }
+            }];
+        }
+    }];
+    touchIdBtn.translatesAutoresizingMaskIntoConstraints = NO;
+    [touchIdBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    touchIdBtn.backgroundColor = [UIColor colorWithRed:0.000 green:0.400 blue:0.800 alpha:1.000];
+    touchIdBtn.layer.cornerRadius = 5.0;
+    touchIdBtn.clipsToBounds = YES;
+    [loginView addSubview:touchIdBtn];
+    
+    NSDictionary *views = NSDictionaryOfVariableBindings(usernameField, passwdField, btn, touchIdBtn);
+    NSDictionary *metrics = @{@"horiBorderDist": @372,
+                              @"vertiBorderDist": @120};
+    NSString *vfl1 = @"|-horiBorderDist-[usernameField]-horiBorderDist-|";
+    NSString *vfl2 = @"|-horiBorderDist-[passwdField(usernameField)]-horiBorderDist-|";
+    NSString *vfl3 = @"|-horiBorderDist-[btn]-8-[touchIdBtn(btn)]-horiBorderDist-|";
+    NSString *vfl4 = @"V:|-vertiBorderDist-[usernameField]-16-[passwdField(usernameField)]-16-[btn(40)]";
+    NSString *vfl5 = @"V:[passwdField]-16-[touchIdBtn(40)]";
+    for (NSString *tmpVFL in @[vfl1, vfl2, vfl3, vfl4, vfl5]) {
+        [loginView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:tmpVFL options:0 metrics:metrics views:views]];
+    }
+    
+    [self.view addSubview:loginView];
 }
 
 /*
